2cfa03c92d196216851b85300d36f1a1e910029e
Owen Dodd <owen.dodd@gmail.com>
2018-02-19 00:43:54 -0500

    All New

--- script.js
+++ script.js
@@ -10,20 +10,37 @@
 //   return false
 // })
 
-$(document).ready(function(){
+$(document).ready(function() {
 
 
 
-$(document).mousemove(function(event) {
+  $(document).mousemove(function(event) {
 
-        if (event.clientY<150) {
+    if (event.clientY < 120) {
 
-          $('header').addClass("slide");
+      $('header').addClass("slideDown");
 
-        } else {
+    } else {
 
-          $('header').removeClass("slide");
-        }
+      $('header').removeClass("slideDown");
+    }
 
-      })
-    });
+  })
+
+
+  $(document).scroll(function(event) {
+
+    if(window.pageYOffset>200) {
+
+      $('header').addClass("slideUp");
+
+    } else {
+
+      $('header').removeClass("slideUp");
+    }
+
+  })
+
+
+
+});
